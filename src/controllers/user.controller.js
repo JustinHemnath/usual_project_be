06ff9af666340d14f40f40111bece82da545feba
Hemnath Balasubramanian <hemnath@theproindia.com>
2025-01-22 19:11:26 +0530

    replaced drizzle with prisma

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { db } from "../db/db.js";
 import { users, messages } from "../db/schema.js";
-import { and, eq } from "drizzle-orm";
+import { and, eq, or } from "drizzle-orm";
+import fs from "node:fs";
 
 export async function getUsers(req, res) {
   const usersData = await db.query.users.findMany();
@@ -10,37 +11,55 @@ export async function getUsers(req, res) {
 
 export async function validateUser(req, res) {
   const userToValidate = req.body?.user;
+  console.log({ tri: userToValidate });
 
-  if (!userToValidate || userToValidate?.name || userToValidate?.email) {
-    res.status(400).send("User details not sent");
+  if (!userToValidate || !userToValidate?.name || !userToValidate?.email) {
+    return res.status(400).send("User details not sent");
   }
 
   const users = await db.query.users.findMany();
-  const isMailRegistered = users.find((user) => user.email === userToValidate.email);
 
-  if (!isMailRegistered) {
+  console.log({ users });
+
+  if (users.length === 0) {
     await db.insert(users).values({ name: userToValidate.name, email: userToValidate.email });
+    console.log("Inserted");
 
-    res.status(200).send({
+    return res.status(200).send({
       action: "registered",
       metaData: {
         users,
       },
     });
   } else {
-    const currentUserEmail = userToValidate.email;
-    const allUserMessages = await db
-      .select()
-      .from(messages)
-      .where(and(eq(messages.sender, currentUserEmail), eq(messages.receiver, currentUserEmail)));
+    const isMailRegistered = users.find((user) => user.email === userToValidate.email);
 
-    res.status(200).send({
-      action: "validated",
-      metaData: {
-        users,
-        messages: allUserMessages,
-      },
-    });
+    console.log({ isMailRegistered });
+
+    if (!isMailRegistered) {
+      await db.insert(users).values({ name: userToValidate.name, email: userToValidate.email });
+
+      return res.status(200).send({
+        action: "registered",
+        metaData: {
+          users,
+        },
+      });
+    } else {
+      const currentUserEmail = userToValidate.email;
+      const allUserMessages = await db
+        .select()
+        .from(messages)
+        .where(or(eq(messages.sender, currentUserEmail), eq(messages.receiver, currentUserEmail)));
+
+      return res.status(200).send({
+        action: "validated",
+        metaData: {
+          users,
+          messages: allUserMessages,
+        },
+      });
+    }
   }
 }
 
@@ -50,9 +69,9 @@ export async function postMessages(req, res) {
   const allUserMessages = await db
     .select()
     .from(messages)
-    .where(and(eq(messages.sender, currentUserEmail), eq(messages.receiver, currentUserEmail)));
+    .where(or(eq(messages.sender, currentUserEmail), eq(messages.receiver, currentUserEmail)));
 
-  console.log({ allUserMessages });
+  fs.writeFileSync("./test.json", JSON.stringify(allUserMessages));
 
   res.status(200).send("HELLOOOO");
 }
